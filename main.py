1bdc4b628c2f927e4841ca6d7ee966d2088924b4
Aadyanjali Daita <89221238+AadyaDaita@users.noreply.github.com>
2022-03-30 05:12:03 +0000

    main.py and menu.py up and running (the repl was not running before, but now it is rendering)

--- main.py
+++ main.py
@@ -1,48 +1,120 @@
-from flask import render_template, request
-from __init__ import app
+main_menu = []
 
-from algorithm.algorithm import algorithm_bp
-from crud.crud import model_bp
-from recipe.recipe import recipe_bp
-from restapi.restapi import restapi_bp
-from starter.starter import starter_bp
-from y2022 import y2022_bp
+# Submenu list of [Prompt, Action]
+# Works similarly to main_menu
+data_sub_menu = [
+    ["Swap", "python_menu_challenges/week_0/swap.py"],
+    ["Keypad", "python_menu_challenges/week_0/keypad.py"],
+    ["Lists", "python_menu_challenges/week_1/lists_and_loops.py"]
+]
 
-#hello!
-@app.route('/')
-def index():
-    return render_template("index.html")
+math_sub_menu = [
+    ["Fibonacci", "python_menu_challenges/week_1/fibonacci.py"],
+    ["Factorials", "python_menu_challenges/week_2/factorial.py"],
+    ["Factors", "python_menu_challenges/week_2/factors_num.py"],
+    ["Palidrome", "python_menu_challenges/week_2/palidrome.py"]
+  ]
 
+patterns_sub_menu = [
+    ["Christmas", "python_menu_challenges/week_0/christmastree.py"],
+    ["Ship", "python_menu_challenges/week_0/ship.py"],
+    
+  
+   
+  ]
+# def menu
+# using main_menu list:
+# 1. main menu and submenu reference are created [Prompts, Actions]
+# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
+# Menu banner is typically defined by menu owner
+border = "=" * 25
+banner = f"\n{border}\nplease pick one\n{border}"
+# def menu
+# using main_menu list:
+# 1. main menu and submenu reference are created [Prompts, Actions]
+# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
+def menu():
+    title = "function menu" + banner
+    menu_list = main_menu.copy()
+    menu_list.append(["Data", data])
+    menu_list.append(["Math", math])
+    menu_list.append(["Patterns", pattern])
+    buildMenu(title, menu_list)
+# def submenu
+# using sub menu list above:
+# sub_menu works similarly to menu()
+def data():
+    title = "function submenu" + banner
+    buildMenu(title, data_sub_menu)
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    # submit button has been pushed
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("greet.html", name=name)
-    # starting and empty input default
-    return render_template("greet.html", name="World")
+def math():
+    title = "Function Submenu" + banner
+    buildMenu(title, math_sub_menu)
 
-
-@app.route('/deploy')
-def deploy():
-    return render_template("course/deploy.html")
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
-
-
-app.register_blueprint(algorithm_bp)
-app.register_blueprint(model_bp)
-app.register_blueprint(recipe_bp)
-app.register_blueprint(restapi_bp)
-app.register_blueprint(starter_bp)
-app.register_blueprint(y2022_bp)
-
+def pattern():
+    title = "Function Submenu" + banner
+    buildMenu(title, patterns_sub_menu)
+  
+def buildMenu(banner, options):
+    # header for menu
+    print(banner)
+    # build a dictionary from options
+    prompts = {0: ["Exit", None]}
+    for op in options:
+        index = len(prompts)
+        prompts[index] = op
+    # print menu or dictionary
+    for key, value in prompts.items():
+        print(key, '->', value[0])
+    # get user choice
+    choice = input("input:")
+    
+    # validate choice and run
+    # execute selection
+    # convert to number
+    try:
+        choice = int(choice)
+      
+        
+          
+        if choice == 0:
+            # stop
+            print("you have left the program! thank you!")
+            exit()
+            return
+            
+        try:
+            # try as function
+            action = prompts.get(choice)[1]
+            
+            action()
+            
+            
+        except TypeError:
+            try:  # try as playground style
+                
+                exec(open(action).read())
+                
+                
+     
+                #python_menu_challenges/ship.ship()
+                
+          
+            except FileNotFoundError:
+                print(f"File not found!: {action}")
+            # end function try
+        # end prompts try
+    except ValueError:
+        # not a number error
+        print(f"Not a number: {choice}")
+    except UnboundLocalError:
+        # traps all other errors
+        print(f"Invalid choice: {choice}")
+    # end validation try
+    buildMenu(banner, options)  # recursion, start menu over again
 if __name__ == "__main__":
-    # runs the application on the repl development server
-    app.run(debug=True, port="5222")
+    menu()
+
+#test
+
+
--- palindrome.md
+++
@@ -1,9 +0,0 @@
-{% include navigation.html %}
-
-
-| Task | Replit Runtime Link | Code Commit | 
-| --- | --- | --- |
-| Palindrome | [Replit runtime](https://replit.com/@GennalynBongola/Palindrome#main.py) | [Commit](https://github.com/Gennalynb123/Individual-Algorithmic-Project/blob/7d5b6f24b1be3844220c7c8c81e405df72764830/templates/palindrome.py) |
-
-
-{% include palindrome.html %}
